4ae02d34bfbbac1ebca658473169519d69cd1e4e
ahmed elkomy <ahmedelkomy@gmail.com>
2023-10-09 19:27:34 +0300

    first_design change_icon new activity

---
+++ app/src/main/java/com/ahmed/myapplication/quick_Servce.kt
@@ -0,0 +1,11 @@
+package com.ahmed.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class quick_Servce : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quick_servce)
+    }
+}
