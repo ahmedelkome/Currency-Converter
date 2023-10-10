e823b38d2ad530876e46950ce16f73c7b5b06777
ahmed elkomy <ahmedelkomy@gmail.com>
2023-10-10 04:14:05 +0300

    new activity

---
+++ app/src/main/java/com/ahmed/myapplication/Currency_Converter.kt
@@ -0,0 +1,11 @@
+package com.ahmed.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Currency_Converter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_currency_converter)
+    }
+}
