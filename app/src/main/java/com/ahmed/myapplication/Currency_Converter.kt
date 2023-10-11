1af9ecbaf2d9c8677c909f9516cc9ff8400fdb15
ahmed elkomy <ahmedelkomy@gmail.com>
2023-10-11 04:13:36 +0300

    FINISH CURRENCY APP

--- app/src/main/java/com/ahmed/myapplication/Currency_Converter.kt
+++ app/src/main/java/com/ahmed/myapplication/Currency_Converter.kt
@@ -12,25 +12,41 @@ class Currency_Converter : AppCompatActivity() {
     val egypt ="Egyption pound"
     val america ="American dollar"
     val AED="AED"
-    val GED="GED"
+    val GBP="GBP"
+    val values = mapOf(
+        america to 1.0 ,
+        egypt to 30.90 ,
+        AED to 8.41 ,
+        GBP to 38.01
+    )
+    lateinit var  convert : Button
+    lateinit var amountEt : TextInputEditText
+    lateinit var resultEt : TextInputEditText
+    lateinit var list:AutoCompleteTextView
+    lateinit var from : AutoCompleteTextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_currency_converter)
-        val convert:Button = findViewById(R.id.conv_b)
-        val amountEt:TextInputEditText = findViewById(R.id.amount)
-        val resultEt : TextInputEditText = findViewById(R.id.result)
-        val list:AutoCompleteTextView =findViewById(R.id.list2)
-        val listofcountry= listOf(egypt,america,AED,GED)
-        val adapter = ArrayAdapter(this,R.layout.text_file,listofcountry)
-        list.setAdapter(adapter)
-
+        description()
+        country()
         convert.setOnClickListener {
             val amount = amountEt.text.toString().toDouble()
+            val tovalue =values [list.text.toString()]
+            val fromvalue = values[from.text.toString()]
+            val result = amount.times(tovalue!!.div(fromvalue!!))
+            resultEt.setText(result.toString())
+        }
+
+
+
+
+
+    /*  val amount = amountEt.text.toString().toDouble()
             val country = list.text.toString()
             var result = when (country){
                 egypt -> amount.times(30.90)
                 AED -> amount.times(3.67)
-                GED -> amount.times(0.82)
+                GBP -> amount.times(0.82)
                 else -> {
                     amount.times(1)
                 }
@@ -38,7 +54,20 @@ class Currency_Converter : AppCompatActivity() {
 
             resultEt.setText(result.toString())
 
-        }
+        }*/
 
+    }
+    private fun description(){
+        convert  = findViewById(R.id.conv_b)
+        amountEt = findViewById(R.id.amount)
+        resultEt = findViewById(R.id.result)
+        list = findViewById(R.id.list2)
+        from = findViewById(R.id.from)
+    }
+    private fun country(){
+        val listofcountry= listOf(egypt,america,AED,GBP)
+        val adapter = ArrayAdapter(this,R.layout.text_file,listofcountry)
+        list.setAdapter(adapter)
+        from.setAdapter(adapter)
     }
 }
